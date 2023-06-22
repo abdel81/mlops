073d7880027ebc95eb9eef32b703bc2f28ebc3e2
Groupe5 <ouardi.abdellatif@gmail.com>
2023-06-22 01:06:31 +0100

    Adding cookiecutter template

---
+++ churn_model/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='projet MLOPS pour la prediction de la performance des etudiants',
+    author='abdellatif',
+    license='MIT',
+)
